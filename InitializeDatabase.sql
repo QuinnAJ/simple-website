de8048b1b0d726255a4dac5ec0872156acb1889c
Anthony Quinn <quinn_anthony@hotmail.com>
2018-01-21 12:05:21 -0500

    File for creating database

---
+++ InitializeDatabase.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE wp_eatery;
+GRANT USAGE ON *.* TO wp_eatery@localhost IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON wp_eatery.* TO wp_eatery@localhost;
+FLUSH PRIVILEGES;
+
+USE wp_eatery;
+
+CREATE TABLE mailingList(
+	_id int not null AUTO_INCREMENT,
+	firstName VARCHAR(50) NOT NULL,
+	lastName VARCHAR(50) NOT NULL,
+	phoneNumber VARCHAR(15) NOT NULL,
+	emailAddress VARCHAR(100) NOT NULL,
+	username VARCHAR(50) NOT NULL,
+	referrer VARCHAR(15) NOT NULL,
+	PRIMARY KEY (_id)
+	);
